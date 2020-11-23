c1ad2a984d3bab9b5fc0f018ffa9ddbbddf2699a
Mirko <mirko.petrelli@nexor.it>
2020-11-23 16:42:12 +0100

    Configurazione Angular build

---
+++ ng-ogame/ogame-ext/src/assets/content/content.js
@@ -0,0 +1,84 @@
+console.debug("Content js is running...");
+
+function prova(a){
+    console.debug("prova", a );
+}
+
+var interval = null;
+
+/*
+    - maxBet
+    - timer
+ */
+function bot(maxBet, timer) {
+
+    var playerNameChilds = document.querySelectorAll('#playerName span');
+    var playerName = playerNameChilds[playerNameChilds.length - 1].textContent.trim();
+    console.debug("Player Name: ", playerName);
+
+    var [inputMet, inputCry, inputDeu] = document.querySelectorAll(".normalResource input");
+
+    console.debug("Input met:", inputMet);
+    console.debug("Input cry:", inputCry);
+    console.debug("Input deu:", inputDeu);
+
+    var [maxBtnMet, maxBtnCry, maxBtnDeu] = document.querySelectorAll(".normalResource a.max");
+
+    console.debug("Max btn met:", maxBtnMet);
+    console.debug("Max btn cry:", maxBtnCry);
+    console.debug("Max btn deu:", maxBtnDeu);
+
+    var payBtn = document.querySelectorAll("a.pay")[0];
+    console.debug("Pay btn", payBtn);
+
+    var currentBetEl = document.querySelectorAll(".detail_value.odd.currentSum")[0];
+    console.debug("Current Bet El:", currentBetEl);
+
+    var currentPlayerEl = document.querySelectorAll('.currentPlayer')[0];
+    console.debug("Current Player El:", currentPlayerEl);
+
+    function getCurrentPlayerName() {
+        return currentPlayerEl.textContent.trim();
+    }
+
+    function getCurrentBet() {
+        return Number.parseInt(currentBetEl.textContent.replaceAll('.', ''));
+    }
+
+    function submit() {
+        console.debug("Pay click");
+        myBet = inputMet.value;
+        payBtn.click();
+    }
+
+
+    console.debug("Max bet:", maxBet);
+    console.debug("Player Name:", playerName);
+
+    interval = setInterval(() => {
+
+        if (getCurrentPlayerName() === playerName) {
+            myBet = getCurrentBet();
+            console.debug("Current Player match your name");
+            return;
+        }
+
+        if (getCurrentBet() >= maxBet) {
+            console.debug("Current bet is over the limit");
+            stop();
+            return;
+        }
+
+        console.debug("Max click");
+        maxBtnMet.click();
+
+        submit();
+    }, timer || 1050);
+}
+
+function stop() {
+    console.debug("STOP");
+    clearInterval(interval);
+}
+
+
