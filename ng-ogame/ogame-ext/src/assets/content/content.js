4ec4c206bdf33978f6fadf73cc47fbdf27088d1f
Mirko Petrelli <25347621+Mirko92@users.noreply.github.com>
2020-12-04 23:27:19 +0100

    Pulsanti di prova

--- ng-ogame/ogame-ext/src/assets/content/content.js
+++ ng-ogame/ogame-ext/src/assets/content/content.js
@@ -16,7 +16,23 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
+function addFleetsButton() {
+    const planets = document.querySelectorAll('#planetList > div');
 
+    planets.forEach(p => {
+        p.appendChild(fleetButton());
+    });
+};
+
+function fleetButton() {
+    let button = document.createElement('button');
+    button.textContent = "FS(m)";
+    button.classList = 'mp_fleet_button fleet_icon_forward_end';
+    button.title = 'Run fleet save';
+    return button;
+}
+
+addFleetsButton();
 
 /*
     Aggiungo un elemento script alla pagina
@@ -24,51 +40,65 @@ chrome.runtime.onMessage.addListener(
  */
 var scriptEL = document.createElement("script");
 scriptEL.innerHTML = `
-    window.mp = {
-        init:function(){
-            console.debug("Init ogame extension");
+window.mp = {
+    init: function () {
+        console.debug("Init ogame extension");
 
-            console.debug("Player name: ", player.name);
+        console.debug("Player name: ", player.name);
 
-            console.debug("Current page: ", currentPage);
+        console.debug("Current page: ", currentPage);
 
 
-            switch (currentPage) {
-                case "fleetdispatch":
-                    localStorage.setItem('mp_fleet', shipsData);
-                    break;
-            
-                default:
-                    break;
-            }
+        switch (currentPage) {
+            case "fleetdispatch":
+                // TODO: Distinct by Coords and planet type
+                localStorage.setItem('mp_fleet', shipsData);
+                break;
 
-        },
-
-        goToFleet(){
-            window.location = 'index.php?page=ingame&component=fleetdispatch';
-        },
-
-        message(txt, isAlert){
-            fadeBox(txt, isAlert);
+            default:
+                break;
         }
-    }
 
-    window.onload = mp.init();
+        this.addFleetActions();
+    },
+
+    addFleetActions(){
+        console.debug("Aggiungo azioni ai pulsanti di fleet save");
+
+        const fleetButtons = document.querySelectorAll('.mp_fleet_button');
+
+        console.debug("Trovati " + fleetButtons.length + " pulsanti");
+
+        fleetButtons.forEach(btn => btn.onclick = this.quickFleetSave);
+    },
+
+    quickFleetSave(event){
+        console.log("prova", event);
+    },
+
+    goToFleet() {
+        window.location = 'index.php?page=ingame&component=fleetdispatch';
+    },
+
+    message(txt, isAlert) {
+        fadeBox(txt, isAlert);
+    }
+}
+
+window.onload = mp.init();
 `;
 
 document.head.appendChild(scriptEL);
 
 function prova(a) {
     console.debug("prova", a);
-
-
 }
 
 var interval = null;
 
 /*
-    - maxBet
-    - timer
+    - maxBet: number 
+    - timer: nubmer (milliseconds)
  */
 function bot(maxBet, timer) {
 
--- scriptToLoad.js
+++ scriptToLoad.js
@@ -1,32 +1,48 @@
-`
-    window.mp = {
-        init:function(){
-            console.debug("Init ogame extension");
+window.mp = {
+    init: function () {
+        console.debug("Init ogame extension");
 
-            console.debug("Player name: ", player.name);
+        console.debug("Player name: ", player.name);
 
-            console.debug("Current page: ", currentPage);
+        console.debug("Current page: ", currentPage);
 
 
-            switch (currentPage) {
-                case "fleetdispatch":
-                    localStorage.setItem('mp_fleet', shipsData);
-                    break;
-            
-                default:
-                    break;
-            }
+        switch (currentPage) {
+            case "fleetdispatch":
+                // TODO: Distinct by Coords and planet type
+                localStorage.setItem('mp_fleet', shipsData);
+                break;
 
-        },
-
-        goToFleet(){
-            window.location = 'index.php?page=ingame&component=fleetdispatch';
-        },
-
-        message(txt, isAlert){
-            fadeBox(txt, isAlert);
+            default:
+                break;
         }
-    }
 
-    window.onload = mp.init();
-`;
+        this.addFleetsButton();
+
+    },
+
+    addFleetsButton() {
+        const planets = document.querySelectorAll('#planetList > div');
+
+        planets.forEach(p => {
+            p.appendChild(this.fleetButton());
+        });
+    },
+
+    fleetButton() {
+        let button = document.createElement('button');
+        button.textContent = "FS(m)";
+        button.classList = ['mp_fleet_button'];
+        return button;
+    },
+
+    goToFleet() {
+        window.location = 'index.php?page=ingame&component=fleetdispatch';
+    },
+
+    message(txt, isAlert) {
+        fadeBox(txt, isAlert);
+    }
+}
+
+window.onload = mp.init();
