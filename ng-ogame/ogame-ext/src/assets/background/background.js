05fdaddd692505cc68b0efff77d82ec6654aa227
Mirko Petrelli <25347621+Mirko92@users.noreply.github.com>
2020-12-07 10:50:25 +0100

    Save

--- ng-ogame/ogame-ext/src/assets/background/background.js
+++ ng-ogame/ogame-ext/src/assets/background/background.js
@@ -38,6 +38,18 @@ chrome.runtime.onInstalled.addListener(function () {
   ]);
 });
 
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (var key in changes) {
+    var storageChange = changes[key];
+    console.debug(
+      'Storage key "%s" in namespace "%s" changed. ' +
+      'Old value was "%s", new value is "%s".',
+      key,
+      namespace,
+      storageChange.oldValue,
+      storageChange.newValue);
+  }
+});
 
 //#region MESSAGES HANDLING
 console.debug("#######################################");
@@ -54,6 +66,28 @@ chrome.runtime.onMessageExternal.addListener(
     switch (request.method) {
       case "SAVE_FLEET_INFO":
         console.debug("Methdod, save fleet info", request.data);
+        const { uni, planet, shipsData } = request.data;
+
+
+        chrome.storage.local.get([uni], function (result) {
+          const value = (result[uni] || {});
+          console.debug('Value currently is ', value);
+
+          const { galaxy, system, position, type } = planet;
+          const coords = galaxy + "_" + system + "_" + position + "_" + type;
+
+          console.debug("Updating coords:", coords);
+
+          value[coords] = {
+            ...(value[coords] || {}),
+            shipsData
+          };
+
+          chrome.storage.local.set({ [uni]: value }, function () {
+            console.log('Value is set to ' + value);
+          });
+        });
+
         break;
       case "GET_FLEET_INFO":
         console.debug("Methdod, get fleet info");
