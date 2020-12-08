0213a3916858e19a156bf6cad4509f7eddc0a29b
Mirko Petrelli <25347621+Mirko92@users.noreply.github.com>
2020-12-08 11:02:48 +0100

    No

--- scriptToLoad.js
+++ scriptToLoad.js
@@ -1,28 +1,34 @@
-const MISSIONS = {
-    ATTACK = 1,
+// TODO: Trovare un modo per caricarlo, ultimo aggiornamento 11.02 del 8 Dic. 2020
+
+const MP_MISSIONS = {
+    ATTACK: 1,
     UNIONATTACK: 2,
     TRANSPORT: 3,
     DEPLOY: 4,
     HOLD: 5,
-    ESPIONAGE = 6,
-    COLONIZE = 7,
-    RECYCLE = 8,
+    ESPIONAGE: 6,
+    COLONIZE: 7,
+    RECYCLE: 8,
     DESTROY: 9,
     MISSILEATTACK: 10,
     EXPEDITION: 15
 };
 
-const PLANET_TYPES = {
+const MP_PLANET_TYPES = {
     PLANET: 1,
     DEBRIS: 2,
     MOON: 3
-}
+};
 
 window.mp = {
     server: () => String,
 
     extensionId() {
         return localStorage.getItem('mp_ogame_ext_id');
+    },
+
+    fleetToken(){
+        return localStorage.getItem('mp_fleet_token');
     },
 
     /**
@@ -47,37 +53,59 @@ window.mp = {
         const planet = this.getAttribute('data-planet');
         const moon = this.getAttribute('data-moon');
 
-        const body = {
-            mission: MISSIONS.TRANSPORT,
+        const missionParams = new URLSearchParams({
+            token: this.fleetToken,
+            speed:10,
+            mission: MP_MISSIONS.TRANSPORT,
             //TO:
             galaxy: 1,
             system: 299,
             position: 6,
-            type: PLANET_TYPES.MOON,
+            type: MP_PLANET_TYPES.MOON,
+            //HOLD:
+            metal:1000,
+            crystal:0,
+            deuterium:0,
 
-        }
+            prioMetal:1,
+            prioCrystal:2,
+            prioDeuterium:3,
 
-        // fetch("https://s172-it.ogame.gameforge.com/game/index.php?page=minifleet&ajax=1", {
-        //     "headers": {
-        //         "accept": "application/json, text/javascript, */*; q=0.01",
-        //         "accept-language": "it,it-IT;q=0.9,en;q=0.8,en-US;q=0.7",
-        //         "cache-control": "no-cache",
-        //         "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        //         "pragma": "no-cache",
-        //         "sec-fetch-dest": "empty",
-        //         "sec-fetch-mode": "cors",
-        //         "sec-fetch-site": "same-origin",
-        //         "x-requested-with": "XMLHttpRequest"
-        //     },
-        //     "referrer": "https://s172-it.ogame.gameforge.com/game/index.php?page=ingame&component=overview",
-        //     "referrerPolicy": "strict-origin-when-cross-origin",
-        //     "body": "mission=6&galaxy=1&system=89&position=4&type=3&shipCount=10&token=3e0f2d281f404f7ca572d646186ac3d8",
-        //     "method": "POST",
-        //     "mode": "cors",
-        //     "credentials": "include"
-        // }).then(r => console.debug(r));
+            retreatAfterDefenderRetreat:0,
+            union:0,
+            holdingtime:0,
 
-        fadeBox('Ci proviamo');
+            //Ships
+            am203:1,
+        }).toString();
+
+        let fleetUrl = ogameUrl+"/game/index.php?page=ingame&component=fleetdispatch&action=sendFleet&ajax=1&asJson=1";
+
+        fetch(fleetUrl, {
+            "headers": {
+                "accept": "application/json, text/javascript, */*; q=0.01",
+                "accept-language": "it,it-IT;q=0.9,en;q=0.8,en-US;q=0.7",
+                "cache-control": "no-cache",
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "pragma": "no-cache",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-requested-with": "XMLHttpRequest"
+            },
+            "referrer": fleetUrl,
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": missionParams,
+            "method": "POST",
+            "mode": "cors",
+            "credentials": "include"
+        }).then(x => {
+            x.json().then(body => {
+                const response = body.response;
+                fadeBox(response.message, !response.success);
+                miniFleetToken = body.newToken;
+            })
+        });
     },
 
     /**
@@ -110,6 +138,8 @@ window.mp = {
 
         switch (currentPage) {
             case "fleetdispatch":
+                // TODO: Gestione local storage in file a parte 
+                localStorage.setItem('mp_fleet_token', fleetDispatcher.fleetSendingToken);
                 this.saveFleetInfo(this.server, currentPlanet, shipsOnPlanet);
                 break;
             default:
