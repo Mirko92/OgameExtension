be7b2b013a9e1062e40ce2fdfa8379b7d88ab977
Mirko <mirko.petrelli@nexor.it>
2020-12-09 13:08:34 +0100

    Update

--- ng-ogame/ogame-ext/apps/option/src/app/app.component.ts
+++ ng-ogame/ogame-ext/apps/option/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'option';
+
+  storage: any = { "PROVA": "ASD" };
+
+  keysOf(arg){
+    return Object.keys(arg);
+  }
+
+  testo = "TESTO";
+
+  ngOnInit(): void {
+    this.initStorage();
+  }
+
+  initStorage() {
+    console.debug("ci provos");
+
+    const promise = new Promise((resolve) => {
+      chrome.storage.local.get(null, resolve);
+    });
+
+    promise.then(r => {
+      this.storage = r;
+    });
+
+  }
 }
