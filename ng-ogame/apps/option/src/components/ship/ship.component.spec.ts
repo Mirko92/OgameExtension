9c8a16010656a00ed12252f0804ec5ab5b44437c
Mirko <mirko.petrelli@nexor.it>
2020-12-21 17:38:40 +0100

    Angular 11

--- ng-ogame/apps/option/src/components/ship/ship.component.spec.ts
+++ ng-ogame/apps/option/src/components/ship/ship.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ShipComponent } from './ship.component';
 
@@ -6,7 +6,7 @@ describe('ShipComponent', () => {
   let component: ShipComponent;
   let fixture: ComponentFixture<ShipComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ShipComponent ]
     })
