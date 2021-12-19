1896e1a302aba8be92b6b27fcf367599b2bac688
Mirko <mirko.petrelli@outlook.it>
2021-12-19 19:03:48 +0100

    Icons, name, description

--- src/manifest.ts
+++ src/manifest.ts
@@ -11,7 +11,7 @@ export async function getManifest(): Promise<Manifest.WebExtensionManifest> {
     version: pkg.version,
     description: pkg.description,
     action: {
-      default_icon: './assets/icon-512.png',
+      default_icon: './assets/icon_128.png',
       default_popup: './dist/popup/index.html',
     },
     options_ui: {
@@ -28,9 +28,10 @@ export async function getManifest(): Promise<Manifest.WebExtensionManifest> {
       },
     ],
     icons: {
-      16: './assets/icon-512.png',
-      48: './assets/icon-512.png',
-      128: './assets/icon-512.png',
+      16:   "./assets/icon_16.png",
+      32:   "./assets/icon_32.png",
+      48:   "./assets/icon_48.png",
+      128:  "./assets/icon_128.png"
     },
     permissions: [
       'tabs',
