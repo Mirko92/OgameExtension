10c4d2d15c4a35758aacc9a2375c62ae890ab123
Mirko <mirko.petrelli@nexor.it>
2022-02-03 15:59:17 +0100

    Settings draft

--- views/options/MpSettings.vue
+++ views/options/MpSettings.vue
@@ -1,25 +1,65 @@
 <script lang="ts" setup>
-
+    const deuReserve = ref(1000)
+    const banner = ref(false)
+    const gfBar = ref(false)
 </script>
 
 <template>
 <div class="page">
-    <header>
-        <h1 v-t="'mp_settings.title'">Settings</h1>
-    </header>
-
-    <h3>TODO</h3>
-    <ul>
-        <li>Deuterio da lasciare su luna</li>
-        <li>CSS configurarion per pubblicità e barra gf</li>
-    </ul>
     <div class="mp_card">
-        <h3>Riserva di deuterio</h3>
-        <div>Riserva di deuterio che verrà esclusa dai trasporti automatici</div>
-        <input 
-            type="number" 
-            class="mp_input"
-        />
+        <header class="text-center">
+            <h1 class="mt05" v-t="'mp_settings.title'" />
+        </header>
+
+        <hr>
+
+        <section>
+            <h3>Riserva di deuterio (K)</h3>
+            <p>Deuterio che intendi conservare su ciascuna colonia/luna</p>
+            <div class="text-center">
+                <input 
+                    type="number" 
+                    class="mp_input"
+                    style="width: 10rem; text-align: center;"
+                    v-model="deuReserve"
+                />
+                <!-- <div><small>Espresso in migliaia</small></div> -->
+            </div>
+        </section>
+
+        <hr>
+
+        <section>
+            <span>Attiva per mostrare i fastidiosissimi banner pubblicitari</span>
+
+            <label for="showBanner" class="d-f-r a-i-c j-c-b">
+                <h3>Mostra banner pubblicitari</h3>
+                <input 
+                    id="showBanner" 
+                    type="checkbox" 
+                    class="mp_input"
+                    v-model="banner" 
+                >
+            </label>
+        </section>
+
+        <hr>
+
+        <section>
+            <span>Attiva per mostrare la barra della GameForge</span>
+
+            <label for="gfBar" class="d-f-r a-i-c j-c-b">
+                <h3>Mostra barra GameForge</h3>
+                <input 
+                    id="gfBar" 
+                    class="mp_input" 
+                    type="checkbox" 
+                    v-model="gfBar" 
+                > 
+            </label>
+        </section>
+
+        <hr>
     </div>
 </div>
 </template>
@@ -34,8 +74,8 @@
 }
 
 .mp_card {
-  background-color: rgba(255, 255, 255, 0.623);
-  backdrop-filter: blur(10px);
+  background-color: rgba(172, 172, 172, 0.623);
+  backdrop-filter: blur(15px);
   border-radius: var(--border-radius);
   padding: var(--default-padding);
 }
